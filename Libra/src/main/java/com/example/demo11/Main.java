0798b63da96150906264c9365b62a2e0ea1d6059
Ehsan <ehsanulhaque2@iut-dhaka.edu>
2023-04-02 17:27:14 +0600

    css

--- Libra/src/main/java/com/example/demo11/Main.java
+++ Libra/src/main/java/com/example/demo11/Main.java
@@ -22,7 +22,9 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         Connection con = connection.fastconnect();
+
         launch(args);
+        //Controller.initialized();
 
     }
 }
